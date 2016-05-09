359887e1258b71f73560c95762bda9ab220534d4
Nicholas Restifo <nsrestifo@hotmail.com>
2016-05-09 19:51:03 -0400

    mainwindow

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Satsu on 5/9/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+    }
+}
---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by Satsu on 5/9/2016.
+ */
+public class MainWindow extends JFrame {
+    private JPanel contentPane;
+
+    public MainWindow() {
+        super();
+        contentPane = new JPanel();
+        this.setContentPane(contentPane);
+    }
+
+
+}
