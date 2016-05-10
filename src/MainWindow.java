b79a791745864f34a892a6d2dda95370406dbff6
Nicholas Restifo <nsrestifo@hotmail.com>
2016-05-09 20:48:50 -0400

    properties

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.io.IOException;
 
 /**
  * Created by Satsu on 5/9/2016.
@@ -8,13 +9,27 @@ public class MainWindow extends JFrame {
 
     public MainWindow() {
         super();
+
+        try {
+            PropertiesManager.load();
+        } catch (IOException e) {
+            try {
+                PropertiesManager.loadDefaultProperties();
+                PropertiesManager.save();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         contentPane = new JPanel();
         this.setContentPane(contentPane);
+        contentPane.setPreferredSize(PropertiesManager.getResolution());
+        pack();
         goToMainMenu();
     }
 
     public void goToMainMenu() {
-
+        // draw main menu stuff here.
     }
 
 
--- src/PropertiesManager.java
+++ src/PropertiesManager.java
@@ -1,3 +1,6 @@
+import com.sun.istack.internal.NotNull;
+
+import java.awt.*;
 import java.io.*;
 import java.nio.file.FileSystemNotFoundException;
 import java.util.Properties;
@@ -6,27 +9,48 @@ import java.util.Properties;
  * Created by Satsu on 5/9/2016.
  */
 public class PropertiesManager {
-    public static final String DEFAULTSAVEPATH = System.getenv("APPDATA") + File.pathSeparator + "thegame";
-
-    private Properties properties;
-
-    public PropertiesManager() {
-        properties = new Properties();
+    public static final String DEFAULTSAVEPATH = System.getenv("APPDATA") + File.separator + "thegame";
+    static {
+        System.out.println(DEFAULTSAVEPATH);
+        File appDir = new File(DEFAULTSAVEPATH);
+        if(!appDir.exists()) {
+            appDir.mkdir();
+        }
     }
 
-    public void save() throws IOException {
+    private static Properties properties = new Properties();
+
+    private PropertiesManager() {}
+
+    public static void save() throws IOException {
         save(DEFAULTSAVEPATH);
     }
 
-    public void save(String filepath) throws IOException {
+    public static void save(String filepath) throws IOException {
         properties.store(new FileOutputStream(new File(filepath)), "");
     }
 
-    public void load() throws IOException {
+    public static void load() throws IOException {
         load(DEFAULTSAVEPATH);
     }
 
-    public void load(String filepath) throws IOException {
+    public static void load(String filepath) throws IOException {
         properties.load(new FileInputStream(new File(filepath)));
+    }
+
+    public static void loadDefaultProperties() {
+        properties = new Properties();
+        setResolution(new Dimension(1024, 768));
+    }
+
+    @NotNull
+    public static void setResolution(Dimension resolution) {
+        String resolutionString = resolution.width + " " + resolution.height;
+        properties.put("resolution", resolutionString);
+    }
+
+    public static Dimension getResolution() {
+        String[] resolution = properties.getProperty("resolution").split("\\s+");
+        return new Dimension(Integer.valueOf(resolution[0]), Integer.valueOf(resolution[1]));
     }
 }
